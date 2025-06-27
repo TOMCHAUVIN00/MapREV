c2744853cd4a56907985e975b7149a0371c24b19
TOMCHAUVIN00 <tchauvin@seeyousun.fr>
2025-06-27 11:15:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-# Remplace par ton lien de téléchargement direct généré
-excel_url = "https://seeyousun-my.sharepoint.com/personal/tchauvin_seeyousun_fr/_layouts/15/download.aspx?SourceUrl=..."
+# Lien Dropbox direct (avec ?dl=1 à la fin)
+dropbox_url = "https://www.dropbox.com/scl/fi/mtsm92eggj6uefnrkmnew/Test-carte.xlsx?rlkey=tkzi1bqb8s7wcusewt47znv0u&st=3476d3gk&dl=1"
 
 try:
-    df = pd.read_excel(excel_url)
-    st.success("Fichier chargé depuis OneDrive/SharePoint.")
+    df = pd.read_excel(dropbox_url)
+    st.success("Fichier chargé depuis Dropbox.")
     st.dataframe(df)
 except Exception as e:
     st.error(f"Erreur lors de la lecture du fichier : {e}")
